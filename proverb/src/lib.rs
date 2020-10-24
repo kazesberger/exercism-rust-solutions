e24518e413c6ca6c07cf0b45de2ddde6cdfc0c07
kazesberger <klaus.azesberger@bearingpoint.com>
2020-10-24 17:25:21 +0200

    Nicer version of proverb using windows

--- proverb/src/lib.rs
+++ proverb/src/lib.rs
@@ -1,11 +1,22 @@
 pub fn build_proverb(list: &[&str]) -> String {
-    let pairs = list.iter().zip(list.iter().skip(1));
-    let all_pairs_rendered = pairs.fold("".to_string(), |a, b| {
-        format!("{}For want of a {} the {} was lost.\n", a, b.0, b.1).to_string()
-    });
-
     match list.first() {
-        Some(val) => all_pairs_rendered + &format!("And all for the want of a {}.", val).to_string(),
-        None => "".to_string()
+        None => String::new(),
+        Some(val) =>
+            list.windows(2)
+                .map(|it| format!("For want of a {} the {} was lost.\n", it[0], it[1]))
+                .chain(std::iter::once(format!("And all for the want of a {}.", val)))
+                .collect(),
     }
 }
+
+// pub fn build_proverb(list: &[&str]) -> String {
+//     let pairs = list.iter().zip(list.iter().skip(1));
+//     let all_pairs_rendered = pairs.fold("".to_string(), |a, b| {
+//         format!("{}For want of a {} the {} was lost.\n", a, b.0, b.1).to_string()
+//     });
+
+//     match list.first() {
+//         Some(val) => all_pairs_rendered + &format!("And all for the want of a {}.", val).to_string(),
+//         None => "".to_string()
+//     }
+// }
